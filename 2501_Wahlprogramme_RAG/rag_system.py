1e6ed852d43a2a804310d0f399fe98db06b148e6
Lisa Kauck <lisa.kauck@stat-up.com>
2025-01-24 17:32:13 +0100

    defined functions in rag_system-py, defined app in streamlit_app.py, one query sent to 7 vectorstorages to retreive 7 answers with respective references

--- 2501_Wahlprogramme_RAG/rag_system.py
+++ 2501_Wahlprogramme_RAG/rag_system.py
@@ -1,135 +1,85 @@
 import os
 from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.vectorstores import FAISS
-from langchain.docstore import InMemoryDocstore
-from langchain_community.embeddings.openai import OpenAIEmbeddings
-import faiss
-import fitz  
-import openai 
-from openai import OpenAI
-import pickle  # For saving FAISS index with metadata
-from openai.error import OpenAIError 
+from langchain_openai import OpenAIEmbeddings
+#from langchain_community.llms import OpenAI
+from langchain_openai import ChatOpenAI
+import openai
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from operator import itemgetter
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+# Party documents
+parties = {
+    "BSW": "pdf/BSW_Wahlprogramm_2025__Entwurf_.pdf",
+    "SPD": "pdf/BTW_2025_SPD_Regierungsprogramm.pdf",
+    "DieLINKE": "pdf/btw_2025_wahlprogramm_die_linke.pdf",
+    "FDP": "pdf/BTW_2025_Wahlprogramm_FDP_Entwurf.pdf",
+    "Gruene": "pdf/BTW_2025_Wahlprogramm_Grüne_Entwurf.pdf",
+    "Union": "pdf/btw_2025_wahlprogramm-cdu-csu.pdf",
+    "AfD": "pdf/Ich_kotze_gleich_Leitantrag-Bundestagswahlprogramm-2025.pdf"
+}
+
 
 # Load environment variables
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+#client = OpenAI(
+#  api_key=os.environ['OPENAI_API_KEY'],  # this is also the default, it can be omitted
+#)
 openai.api_key = OPENAI_API_KEY
 
 # Initialize OpenAI embeddings
-embedding_function = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+embedding_function = OpenAIEmbeddings(model="text-embedding-3-large", api_key=OPENAI_API_KEY)
+# Text-Splitter konfigurieren
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=75)
+# LLM
+llm = ChatOpenAI(temperature=0.7)
 
-dimension = 1536  # Embedding dimension (check the OpenAI model you use, e.g., Ada embeddings have 1536 dimensions)
-index = faiss.IndexFlatL2(dimension)  # Create a FAISS index for L2 (Euclidean) distance
-docstore = InMemoryDocstore({})  # Initialize an empty in-memory document store
-index_to_docstore_id = {}  # Initialize an empty mapping from index to docstore IDs
+def create_vectorstore(path):
+    loader = PyPDFLoader(path)
+    documents = loader.load_and_split(text_splitter)
+    embeddings = embedding_function
+    vectorstore = FAISS.from_documents(documents, embeddings)
+    return vectorstore
 
-# Path to store FAISS index and metadata
-FAISS_INDEX_PATH = "faiss_index_folder"
-METADATA_PATH = "metadata.pkl"
+def setup_retrieval(vectorstore):
+    retriever = vectorstore.as_retriever(search_kwargs={"k":3})
+    return retriever
 
-def load_or_create_faiss_index():
-    """Load an existing FAISS index or create a new one."""
-    global metadata_store
-    if os.path.exists(FAISS_INDEX_PATH) and os.path.exists(METADATA_PATH):
-        faiss_index = FAISS.load_local(FAISS_INDEX_PATH, embedding_function, allow_dangerous_deserialization=True)
-        with open(METADATA_PATH, "rb") as f:
-            metadata_store = pickle.load(f)
-            #metadata_store = json.load(f)
-    else:
-        faiss_index = FAISS(
-            embedding_function=embedding_function,
-            index=index,
-            docstore=docstore,
-            index_to_docstore_id=index_to_docstore_id
-        )
-        metadata_store = {}
-    return faiss_index
+test_prompt =  """Du bist ein kritischer KI-Assistent, der auf Wahlprogramme deutscher Parteien spezialisiert ist.
+                                Gib präzise, klare und fundierte Antworten auf Basis des bereitgestellten Kontextes.
+                                Verschönere nichts, sondern gib nur die Stellungen der jeweiligen Partei wieder.
+                                Wenn du keine Antwort kennst, sage, dass du es nicht weißt.
+                                Wenn du dich auf spezifische Abschnitte oder Themen aus den Wahlprogrammen beziehst, erwähne dies explizit in deiner Antwort.
+                 Kontext:\n{context}\n\n 
+                 Frage:\n {input}\n\n
+                 Bitte geben Sie eine klare Antwort, die sich auf den Kontext stützt, und erwähnen Sie relevante Artikel oder Abschnitte."""
 
-
-faiss_index = load_or_create_faiss_index()
-
-def add_document_to_faiss(party, document_name):
-    """Add the content of a document to the FAISS index."""
-    global metadata_store, faiss_index
-
-    # Process the PDF
-    try:
-        doc = fitz.open(document_name)
-        for i, page in enumerate(doc):
-            text = page.get_text()
-            if text.strip():
-                vector_id = f"{party}_page_{i}"
-                faiss_index.add_texts([text], ids=[vector_id])
-                metadata_store[vector_id] = {"page": i, "source": document_name}
-    except Exception as e:
-        raise Exception(f"Error processing PDF '{document_name}': {e}")
-
-    # Ensure the directory exists
-    os.makedirs(FAISS_INDEX_PATH, exist_ok=True)
-
-    # Save the updated index and metadata
-    faiss_index.save_local(FAISS_INDEX_PATH)
-    with open(METADATA_PATH, "wb") as f:
-        pickle.dump(metadata_store, f)
-        #json.dump(metadata_store, f)
+prompt=ChatPromptTemplate.from_template(test_prompt)
+#print(prompt.invoke)
 
 
-def generate_answer(party, document_name, query, return_sources=False):
-    """
-    Generates an answer for a specific query based on a party's document.
-    """
-    try:
-        # Add document to FAISS if not already added
-        if f"{party}_page_0" not in metadata_store:
-            add_document_to_faiss(party, document_name)
+def invoke_rag_chain(retriever, question):
+    combine_docs_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, combine_docs_chain)
+    answer = rag_chain.invoke({"input": question})
+    return answer
 
-        # Query FAISS
-        results = faiss_index.similarity_search(query, k=3)
+#question = "Wie werden Frauenrechte gestärkt?"
 
-        # Retrieve relevant texts and metadata
-        relevant_texts = [r.page_content for r in results]  
-        context = "\n".join(relevant_texts)
-        if not relevant_texts:
-            return "Keine relevanten Abschnitte gefunden.", []
-
-        messages = [
-            {"role": "system",
-             "content": ("""Du bist ein kritischer KI-Assistent, der auf Wahlprogramme deutscher Parteien spezialisiert ist.
-                         Gib präzise, klare und fundierte Antworten auf Basis des bereitgestellten Kontextes.
-                         Verschönere nichts, sondern gib nur die Stellungen der jeweiligen Partei wieder.
-                         Wenn du dich auf spezifische Abschnitte oder Themen aus den Wahlprogrammen beziehst, erwähne dies explizit in deiner Antwort.""")},
-            {"role": "user",
-             "content": f"Kontext:\n{context}\n\nFrage: {query}\n\nBitte geben Sie eine klare Antwort, die sich auf den Kontext stützt, und erwähnen Sie gegebenenfalls relevante Artikel oder Abschnitte.“"}
-        ]
-
-        response = openai.ChatCompletion.create(
-            model="gpt-4o-mini",
-            messages=messages,
-            temperature=0.7
-        )
-        answer = response["choices"][0]["message"]["content"]
-        
-        if return_sources:
-            sources = [r.metadata for r in results]
-            return answer, sources
-        else:
-            return answer
-    
-    except openai.error.OpenAIError as e:  # Catch OpenAI-specific errors
-        raise Exception(f"OpenAI API error: {e}")
-    except Exception as e:
-        raise Exception(f"Error generating answer: {str(e)}")
-
-def format_source_reference(metadata):
-    """Format the metadata for display."""
-    return f"Source: {metadata['source']}, Page: {metadata['page']}"
-
-if __name__ == "__main__":
-    # Test the system
-    query = "Whas sind die Hauptthemen der Partei?"
-    answer, sources = generate_answer("BSW", "pdf/BSW_Wahlprogramm_2025__Entwurf_.pdf", query, return_sources=True)
-    print("Answer:", answer.content)
-    print("\nSources:")
-    for i, source in enumerate(sources, 1):
-        print(f"\nSource {i}:")
-        print(format_source_reference(source))
+'''for i, j in parties.items():
+    ans = invoke_rag_chain(j, question)
+    print(f"\n########## {i} ###########\n")
+    print(ans.get("answer"))
+    print(f"\n------ Kontext! -------\n")
+    print(ans["context"][0].page_content)
+    print(f"\n------ Page: -------\n")
+    print(ans["context"][0].metadata["page"])
+'''
--- 2501_Wahlprogramme_RAG/streamlit_app.py
+++ 2501_Wahlprogramme_RAG/streamlit_app.py
@@ -1,190 +1,79 @@
 import streamlit as st
-#from rag_system import generate_answer
-import os
-from dotenv import load_dotenv
-from langchain.chains import RetrievalQA
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
-#from langchain_community.llms import OpenAI
-from langchain_openai import ChatOpenAI
-import openai
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from operator import itemgetter
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from rag_system import invoke_rag_chain, parties, create_vectorstore, setup_retrieval
 
 # Set Streamlit page configuration
 st.set_page_config(layout="wide")
-
-# Party documents
-parties = {
-  #  "BSW": "pdf/BSW_Wahlprogramm_2025__Entwurf_.pdf",
-  #  "SPD": "pdf/BTW_2025_SPD_Regierungsprogramm.pdf",
-  #  "DieLINKE": "pdf/btw_2025_wahlprogramm_die_linke.pdf",
-    "FDP": "pdf/BTW_2025_Wahlprogramm_FDP_Entwurf.pdf",
-   # "Green": "pdf/BTW_2025_Wahlprogramm_Grüne_Entwurf.pdf",
-   # "Union": "pdf/btw_2025_wahlprogramm-cdu-csu.pdf",
-   # "AfD": "pdf/Ich_kotze_gleich_Leitantrag-Bundestagswahlprogramm-2025.pdf"
-}
-
-
-# Load environment variables
-load_dotenv()
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-#client = OpenAI(
-#  api_key=os.environ['OPENAI_API_KEY'],  # this is also the default, it can be omitted
-#)
-openai.api_key = OPENAI_API_KEY
-
-# Initialize OpenAI embeddings
-embedding_function = OpenAIEmbeddings(model="text-embedding-3-large", api_key=OPENAI_API_KEY)
-
-def create_vectorstore(path):
-    loader = PyPDFLoader(path)
-    documents = loader.load_and_split()
-    embeddings = embedding_function
-    vectorstore = FAISS.from_documents(documents, embeddings)
-    return vectorstore
-
-def setup_retrieval_qa(vectorstore):
-    llm = ChatOpenAI(temperature=0.7)
-    qa = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever(search_kwargs={"k":5}), chain_type="stuff", return_source_documents=True)
-    return qa
-
-test_prompt =  """Du bist ein kritischer KI-Assistent, der auf Wahlprogramme deutscher Parteien spezialisiert ist.
-                                Gib präzise, klare und fundierte Antworten auf Basis des bereitgestellten Kontextes.
-                                Verschönere nichts, sondern gib nur die Stellungen der jeweiligen Partei wieder.
-                                Wenn du keine Antwort kennst, sage, dass du es nicht weißt.
-                                Wenn du dich auf spezifische Abschnitte oder Themen aus den Wahlprogrammen beziehst, erwähne dies explizit in deiner Antwort.
-                 Kontext:\n{context}\n\n 
-                 Frage:\n {input}\n\n
-                 Bitte geben Sie eine klare Antwort, die sich auf den Kontext stützt, und erwähnen Sie relevante Artikel oder Abschnitte."""
-
-prompt=ChatPromptTemplate.from_template(test_prompt)
-print(prompt.invoke)
-
-def invoke_rag_chain(pdf_path): 
-    rag_chain = (
-        { 
-            "context": itemgetter("question") | create_vectorstore(pdf_path).as_retriever(search_kwargs={"k":3}), 
-            "question":itemgetter("question"),
-            "lang": itemgetter("lang"),
-        }
-        | prompt
-        | ChatOpenAI(temperature=0.7)
-        | StrOutputParser()
-    )
-    question = "Worum geht es? "
-    lang = "German"
-    query = { 
-        "question":question, 
-        "lang":lang,
-    }
-    
-    answer = rag_chain.invoke(query)
-    return answer
-
-#for i, j in parties.items():
- #   ans = invoke_rag_chain(j)
-  #  print(f"\n########## {i} ###########\n")
-   # print(ans)
-
-
-def invoke_rag_chain_2(pdf_path):
-    combine_docs_chain = create_stuff_documents_chain(ChatOpenAI(temperature=0.7), prompt)
-    rag_chain = create_retrieval_chain(create_vectorstore(pdf_path).as_retriever(search_kwargs={"k":3}), combine_docs_chain)
-    answer = rag_chain.invoke({"input": "Wie wird Bildung gestärkt?"})
-    return answer
-
-for i, j in parties.items():
-    ans = invoke_rag_chain_2(j)
-    print(f"\n########## {i} ###########\n")
-    print(ans.get("answer"))
-    print(f"\n------ Kontext! -------\n")
-    print(ans["context"][0].page_content)
-    print(f"\n------ Page: -------\n")
-    print(ans["context"][0].metadata["page"])
-
-
-
-'''
-
 
 # Initialize session state
 if "messages" not in st.session_state:
     st.session_state.messages = {party: [] for party in parties}
 if "party_references" not in st.session_state:
     st.session_state.party_references = {party: [] for party in parties}
+if "vectorstore" not in st.session_state:
+    st.session_state.vectorstore = {party: [] for party in parties}
+if "retriever" not in st.session_state:
+    st.session_state.retriever = {party: [] for party in parties}
+
 
 # Main interface
 st.title("Wahlprogramme Chat Assistant")
 st.write("Chat with German political party programs for 2025!")
 
+#st.subheader("st.session_state object:") 
+#st.session_state
+
 # Dynamic columns for party answers
 columns = st.columns(len(parties))
 for col, (party, document_name) in zip(columns, parties.items()):
-    vectorstore = create_vectorstore(document_name)
-    st.session_state[party] = {
-        "qa_system": setup_retrieval_qa(vectorstore),
-        "vectorstore": vectorstore
-    }
+    vectorestore = create_vectorstore(document_name)
+    st.session_state.vectorstore[party] = vectorestore
+    st.session_state.retriever[party] = setup_retrieval(vectorestore)
 
     with col:
         st.subheader(party)
 
-# Initialize session state
-if "messages" not in st.session_state:
-    st.session_state.messages = {party: [] for party in parties}
-if "party_references" not in st.session_state:
-    st.session_state.party_references = {party: [] for party in parties}
-
-prompt = st.chat_input("Stellen Sie jetzt eine Frage!")
+query = st.chat_input("Stellen Sie jetzt eine Frage!")
 
 for col, (party, document_name) in zip(columns, parties.items()):
     with col:        
-    # Display chat messages from history
+        # Display chat messages from history
         for message in st.session_state.messages[party]:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
-        if prompt:
-            with st.spinner(f"Generiere Antwort für {party}..."):
-                try:
-                    #retriever = vectorstore.as_retriever()
-                    #retrieved_docs = retriever.get_relevant_documents(prompt)
-                    context = "\n".join([doc.page_content for doc in retrieved_docs])
-                    print("--------Check-----")
-                    qa_result = st.session_state[party]["qa_system"].run(prompt)
-                    print(qa_result, "--------Check-----")
-                    answer, references = qa_result["answer"], qa_result["references"]
-                except AttributeError as e:
-                    st.error(f"Fehler: {e}")
-                    qa_result = "Sorry, there was an error processing your request."
 
-            messages = [
-                {"role": "system",
-                    "content": ("""Du bist ein kritischer KI-Assistent, der auf Wahlprogramme deutscher Parteien spezialisiert ist.
-                                Gib präzise, klare und fundierte Antworten auf Basis des bereitgestellten Kontextes.
-                                Verschönere nichts, sondern gib nur die Stellungen der jeweiligen Partei wieder.
-                                Wenn du dich auf spezifische Abschnitte oder Themen aus den Wahlprogrammen beziehst, erwähne dies explizit in deiner Antwort.""")},
-                {"role": "user",
-                    "content": f"Kontext:\n{context}\n\nFrage: {question}\n\nBitte geben Sie eine klare Antwort, die sich auf den Kontext stützt, und erwähnen Sie gegebenenfalls relevante Artikel oder Abschnitte.“"}
-            ]
-            st.session_state.messages[party].append({"role": "user", "content": prompt})
-            st.session_state.messages[party].append({"role": "user", "content": answer})
-            st.session_state.party_references[party] = references
 
-            for message_data in st.session_state.messages[party]:
-                message(message_data["content"], is_user=(message_data["role"] == "user"))
+for col, (party, document_name) in zip(columns, parties.items()):
+    if query:
+        with col:
+            with st.chat_message("user"):
+                st.markdown(query)
+            st.session_state.messages[party].append({"role": "user", "content": query})
+            try:
+                 with st.chat_message("assistant"):
+                    with st.spinner(f"Generiere Antwort für {party}..."):
+                        retriever = st.session_state.retriever[party]
+                        output = invoke_rag_chain(retriever, query)
+                        #answer = output
+                        answer = output["answer"]
+                        context = output["context"]
+                        st.markdown(answer)
+            except AttributeError as e:
+                st.error(f"Fehler! {e}")
+                answer = "Sorry, hat nicht geklappt."
+        st.session_state.messages[party].append({"role": "assistant", "content": answer})
+        st.session_state.party_references[party] = context
 
-            
-            st.markdown("### Referenzen:")
+    # Display current references if existent
+    with col:
+        if st.session_state.party_references[party]:
+            st.markdown("### Referenzen für die aktuelle Antwort:")
             for i, ref in enumerate(st.session_state.party_references[party], 1):
                 with st.expander(f"Referenz {i}"):
-                    st.markdown(f"**Source**: {ref.metadata.get('source', 'N/A')}")
-                    st.markdown(f"**Section**: {ref.metadata.get('section', 'N/A')}")
+                    st.markdown(f"**Seite**: {ref.metadata.get('page', 'N/A')}")
                     st.text(ref.page_content)
+
+            
+
 
 
 # Sidebar
@@ -211,6 +100,7 @@ with st.sidebar:
     st.markdown("### Example Questions")
     example_questions = [
         'Was sind die zentralen Themen im Wahlprogramm von die Partei?',
+        'Verwendet die Partei amtliche Statistiken, um ihre Punkte zu unterstützen?',
         'Welche Maßnahmen schlägt die Partei zur Förderung erneuerbarer Energien vor?',
         'Wie plant die Partei, das Bildungssystem in Deutschland zu verbessern?',
         'Welche Position hat die Partei zur Aufnahme von Geflüchteten?'
@@ -220,25 +110,14 @@ with st.sidebar:
             # Simulate clicking the question
             st.session_state.messages[party].append({"role": "user", "content": question})
             try:
-                qa_result = st.session_state[party]["qa_system"].run(prompt)
-                answer, references = qa_result["answer"], qa_result["references"]
-            except AttributeError as e:
-                st.error(f"Fehler: {e}")
-                qa_result = "Sorry, there was an error processing your request."
-            st.session_state.messages[party].append({"role": "user", "content": prompt})
-            st.session_state.messages[party].append({"role": "user", "content": answer})
-            st.session_state.party_references[party] = references
+                retriever = st.session_state.retriever[party]
+                output = invoke_rag_chain(retriever, query)
+                #answer = output
+                answer = output["answer"]
+                context = output["context"]
+                st.session_state.messages[party].append({"role": "assistant", "content": answer})
+                st.session_state.party_references[party] = context
+                st.rerun()
+            except Exception as e:
+                st.error(f"Fehler! {str(e)}")
 
-            for message_data in st.session_state.messages[party]:
-                message(message_data["content"], is_user=(message_data["role"] == "user"))
-
-            st.session_state.messages[party].append({"role": "user", "content": prompt})
-            
-            st.markdown("### Referenzen:")
-            for i, ref in enumerate(st.session_state.party_references[party], 1):
-                with st.expander(f"Referenz {i}"):
-                    st.markdown(f"**Source**: {ref.metadata.get('source', 'N/A')}")
-                    st.markdown(f"**Section**: {ref.metadata.get('section', 'N/A')}")
-                    st.text(ref.page_content)
-
-'''
