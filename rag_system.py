de9f98d69c6092a642591ddf1b72469ae8f97be2
Lisa-Marie Kauck <104436419+LisaMarieKauck@users.noreply.github.com>
2025-02-18 13:16:16 +0000

    st.secrets()

--- rag_system.py
+++ rag_system.py
@@ -10,13 +10,7 @@ from operator import itemgetter
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_groq import ChatGroq
-from mistralai import Mistral
-
-#embeddings_batch_response = mistral_client.embeddings.create(
-#    model=model,
-#    inputs=["Embed this sentence.", "As well as this one."],
-#)
+import streamlit as st
 
 parties = {
     #"BSW": {'path': "pdf/BSW_Wahlprogramm_2025__Entwurf_.pdf", 'name': "BSW"},
@@ -26,16 +20,6 @@ parties = {
     #"Gruene": {'path': "pdf/BTW_2025_Wahlprogramm_Grüne_Entwurf.pdf", 'name': "Bündnis90/Die Grünen"},
     "Union": {'path': "pdf/btw_2025_wahlprogramm-cdu-csu.pdf", 'name': "Union"},
     #"AfD": {'path': "pdf/Ich_kotze_gleich_Leitantrag-Bundestagswahlprogramm-2025.pdf", 'name': "AfD"}
-}
-
-parties15 = {
-    #"BSW": {'path': "https://bsw-vg.de/wp-content/themes/bsw/assets/downloads/BSW%20Wahlprogramm%202025.pdf", 'name': "BSW"},
-    "SPD": {'path': "https://www.spd.de/fileadmin/Dokumente/Beschluesse/Programm/SPD_Programm_bf.pdf", 'name': "SPD"},
-    "DieLINKE": {'path': "https://www.die-linke.de/fileadmin/user_upload/Wahlprogramm_Langfassung_Linke-BTW25_01.pdf", 'name': "Die LINKE"},
-    "FDP": {'path': "https://www.fdp.de/sites/default/files/2024-12/fdp-wahlprogramm_2025.pdf", 'name': "FDP"},
-    "Gruene": {'path': "https://cms.gruene.de/uploads/assets/20250205_Regierungsprogramm_DIGITAL_DINA5.pdf", 'name': "Bündnis90/Die Grünen"},
-    "Union": {'path': "https://www.politikwechsel.cdu.de/sites/www.politikwechsel.cdu.de/files/downloads/km_btw_2025_wahlprogramm_langfassung_ansicht.pdf", 'name': "Union"},
-    "AfD": {'path': "https://www.afd.de/wp-content/uploads/2025/02/AfD_Bundestagswahlprogramm2025_web.pdf", 'name': "AfD"}
 }
 
 # Party documents
@@ -53,6 +37,8 @@ parties_old = {
 load_dotenv()
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+OPENAI_API_KEY = st.secrets('OPENAI_API_KEY')
 #openai.api_key = OPENAI_API_KEY
 
 # Initialize OpenAI embeddings
